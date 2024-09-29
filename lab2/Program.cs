95656a480581820b3cacb41064604d801540f585
Илья <@DESKTOP-VPI29MR>
2024-09-29 12:56:22 +0200

    Добавьте файлы проекта.

---
+++ lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    Console.WriteLine("Введите число для 1-го катета: ");
+    int x = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число для 2-го катета: ");
+    int y = int.Parse(Console.ReadLine());
+    double gip = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+    double S = 1 * x * y;
+    Console.WriteLine($"Площадь S = {S}");
+    Console.WriteLine($"Гипотенуза gip = {gip:F2}");
+    
+}
+catch
+{
+    Console.WriteLine("Error");
+
+    Console.WriteLine("");
+}
